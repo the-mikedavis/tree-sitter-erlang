b46e7319d08514fca13ac1635ec7966b2c3f8f78
Michael Davis <mcarsondavis@gmail.com>
2022-02-11 21:37:08 -0600

    relabel specs as attributes

--- grammar.js
+++ grammar.js
@@ -44,13 +44,14 @@ module.exports = grammar({
     source: ($) => repeat(choice($._statement, $._expression)),
 
     _statement: ($) =>
-      choice($.function, $.spec, seq(sep($._expression, ","), $._terminator)),
+      choice($.function, alias($._spec, $.attribute), seq(sep($._expression, ","), $._terminator)),
 
     _terminator: ($) => choice(".", "\n"),
 
     function: ($) => seq(sep($._named_stab_clause, ";"), $._terminator),
 
-    spec: ($) => seq("-", "spec", sep($.stab_clause, ";"), $._terminator),
+    _spec: ($) =>
+      seq("-", field("name", alias("spec", $.atom)), sep($.stab_clause, ";"), $._terminator),
 
     // attribute: ($) =>
     //   seq("-", field("name", $._atom), alias(optionalParens($._items), $.arguments), $._terminator),
